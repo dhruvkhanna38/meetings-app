c17489edd35599b96cad59f915df3166129dc8dd
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2020-11-09 02:17:41 +0530

    Duplicate Users Error Resolved

--- src/routers/user.js
+++ src/routers/user.js
@@ -38,31 +38,31 @@ router.get("/checkVerification", async(req, res)=>{
 
 //Signup
 router.post("/users", async (req, res)=>{
-    const newUser = {...req.body,
-                     emailToken:await crypto.randomBytes(64).toString('hex')
-                    }
-    const user = new User(newUser);
-    let fromMail = 'noreply@meetingsApp.com';
-    let toMail = user.email;
-    let subject = "Verify Email";
-    let text = `Verify your account by clicking on the following link -: http://${req.headers.host}/verify-email?token=${user.emailToken}`;
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "dhruvk.me.16@nsit.net.in" ,
-            pass: process.env.GMAIL_PASS
-        }
-        });
-        let mailOptions = {
-            from: fromMail,
-            to: toMail,
-            subject: subject,
-            text: text
-            };
     
     
     try{ 
+        const newUser = {...req.body,
+            emailToken:await crypto.randomBytes(64).toString('hex')
+           }
+        const user = new User(newUser);
+        let fromMail = 'noreply@meetingsApp.com';
+        let toMail = user.email;
+        let subject = "Verify Email";
+        let text = `Verify your account by clicking on the following link -: http://${req.headers.host}/verify-email?token=${user.emailToken}`;
+
+        const transporter = nodemailer.createTransport({
+                                                        service: 'gmail',
+                                                        auth: {
+                                                        user: "dhruvk.me.16@nsit.net.in" ,
+                                                        pass: process.env.GMAIL_PASS
+                                                        }
+                                                        });
+        let mailOptions = {
+                        from: fromMail,
+                        to: toMail,
+                        subject: subject,
+                        text: text
+        };
         await transporter.sendMail(mailOptions);
         //await mg.messages().send(data);
         //await sgMail.send(message);
